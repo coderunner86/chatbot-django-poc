a73a52041a680e0b3b6a5b0f348c1fec85d52278
coderunner86 <java@unicauca.edu.com>
2024-01-31 13:41:44 -0500

    feat: Improve responses with requests - html

--- chatbot_app/home/views.py
+++ chatbot_app/home/views.py
@@ -1,24 +1,47 @@
 import os
 import json
 from django.http import HttpResponse
+from requests_html import HTMLSession
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
-
+from dotenv import load_dotenv
 #scrape_info
 import requests
 from bs4 import BeautifulSoup
-
-#textrazor analyzer
 import textrazor
 import openai
+# Carga las variables de entorno desde el archivo .env
+env_path = os.path.join(".", ".env")
+load_dotenv(dotenv_path=env_path)
 
-api_key = os.getenv("OPENAI_API_KEY")
-
-# os.getenv("TEXTRAZOR")
+# Obtiene la clave de API de OpenAI desde las variables de entorno
+# openai.api_key = os.getenv("OPENAI_API_KEY")
 textrazor.api_key = "f971f680d240076f816fc325b488f1550246be6fed21098d0be8b51e"
+
+def extract_links(url):
+    # Importa la clase HTMLSession desde requests_html
+
+    # Crea una sesión HTML
+    session = HTMLSession()
+
+    # Haz una solicitud GET a la URL de la página que deseas analizar
+    
+    response = session.get(url)
+
+    # Obtiene todos los enlaces de la página
+    links = response.html.absolute_links
+
+    # Crea una lista de tuplas de enlaces similar a url_mappings
+    url_links = [(link, link) for link in links]
+
+    # Imprime la lista de enlaces
+    return url_links
+
+
 def extract_keywords(url):
     client = textrazor.TextRazor(extractors=["entities", "topics"])
     response = client.analyze_url(url)
@@ -32,17 +55,11 @@ def scrape_info(url):
     info = ' '.join(textos)
     return info
 
-def index(request):
-    return HttpResponse("Hello, Django!")
-
-# initialize the lemmatizer and stopwords
-lemmatizer = WordNetLemmatizer()
-stop_words = set(stopwords.words('english'))
-
 url_mapping = {
     'explicar': 'https://www.factcil.com/how-it-works',
     'precios': 'https://www.factcil.com/pricing',
-    'inicio': 'https://www.factcil.com/',
+    'inicio': 'https://www.factcil.com/', 
+    'asesor': 'https://api.whatsapp.com/send/?phone=573012128715&text&type=phone_number&app_absent=0',
 }
 
 @api_view(['POST'])
@@ -52,27 +69,59 @@ def chatbot(request):
     nltk.download('stopwords')
     lemmatizer = WordNetLemmatizer()
     stop_words = set(stopwords.words('spanish'))
+    links=  extract_links(url_mapping['inicio'])
+    tel_link = []
 
+    # Itera sobre los enlaces extraídos
+    for link in links:
+        if link[0].startswith('tel:'):
+            tel_link.append(link[0])
+    # print("extacted links:", links)
+    # print("telefono:", tel_link)
+    
+    keyword = lambda link: link.split('/')[-1]
+
+    # Crea el diccionario usando la palabra clave y los enlaces
+    abs_url = {}
+    for link in links:
+        key = keyword(link[0])  # Utiliza el enlace del primer elemento como clave
+        value = link[1]  # Utiliza el enlace del segundo elemento como valor
+        if key not in abs_url:
+            abs_url[key] = [value]  # Si la clave no existe en el diccionario, crea una nueva lista con el valor
+        else:
+            abs_url[key].append(value)  # Si la clave ya existe en el diccionario, agrega el valor a la lista existente
+            
+    # Imprime el diccionario resultante
+    print("Diccionario de URL mapeadas:")
+    for key, value in abs_url.items():
+        print(key, ":", value)
+    for key in url_mapping:
+    # Verifica si la clave ya existe en abs_url
+        if key not in abs_url:
+            # Si la clave no existe, agrégala con una lista vacía como valor inicial
+            abs_url[key] = []
+    print("Diccionario actualizado:", abs_url)
     message = request.data['message']
     words = nltk.word_tokenize(message.lower())
     words = [word for word in words if word not in stop_words]
     words = [lemmatizer.lemmatize(word) for word in words]
     print("palabras de la consulta: ", words)
-
+    print("openai api key: ", openai.api_key)
     keywords1 = extract_keywords(url_mapping['explicar'])
     keywords2 = extract_keywords(url_mapping['precios'])
     keywords3 = extract_keywords(url_mapping['inicio'])
-    #keywords es una lista de listas
     keywords = keywords1 + keywords2 + keywords3
-    # Utiliza un enfoque de tipo switch para determinar la respuesta
-    for word in words:
-            if word in url_mapping:
+
+    response = ""
+    while True:
+        for word in words:
+            if word in url_mapping or word in abs_url:
                 info = scrape_info(url_mapping[word])
                 related_keywords = extract_keywords(url_mapping[word])
                 print("palabras clave relacionadas: ", related_keywords)
-                system_message = f"Para más información sobre {word}, visita: {url_mapping[word]}"
+               
                 system_messages = f"""
-                        Sigue estos pasos para responder a las consultas de los clientes.
+                    Sigue estos pasos para responder a las consultas de los clientes.
                         La consulta del cliente no tiene un formato, puede ser una palabra o frase,\
                         es decir, no es estructurada y puede tener caracteres especiales o emojí.
 
@@ -95,73 +144,61 @@ def chatbot(request):
                         Solo menciona o referencia informacion de {info} \
                         Responde al cliente en un tono amigable.
 
+                        Paso 5: Adicionalmente al contenido de la respuesta\
+                        siempre responderás de manera informativa\
+                        es decir, con una respuesta que sea escrita en lenguaje natural.\
+                        que trate de identificar si la palabra es escrita en ingles o español\
+                        y responder con un enlace a la informacion.
+
                         Utiliza el siguiente formato:
-                        Respuesta al usuario:<respuesta cortas al cliente tipo Tweets>
+                        Respuesta al usuario:<respuesta de 300 caracteres>
                         Vemos que estas interesado en <razonamiento del paso 1>
                         voy a recabar la informacion <razonamiento del paso 2 o del paso 3>
-
-                        """
-                user_messages = f"""
-                        {message}"""
+                    """
+                user_messages = f"{message}"
 
                 messages = [
-                        {"role": "system", "content": system_messages},
-                        {
-                            "role": "user",
-                            "content": f"{user_messages}",
-                        },
-                    ]
+                    {"role": "system", "content": system_messages},
+                    {"role": "user", "content": f"{user_messages}"},
+                ]
                 prompt_intent = ""
-                    
                 for message in messages:
                     prompt_intent += f"{message['role'].title()}: {message['content']}\n"
-                # print("prompt del chatbot: ", prompt_intent)
+
                 prompt = f"construye una respuesta basada en {prompt_intent}"
-                API_KEY	= os.getenv("OPENAI_API_KEY")
-                openai.api_key = "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"
+                openai.api_key = "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"
                 option_role = messages[1]['role']
                 option_content = messages[1]['content']
-     
+
                 while not option_content == "exit":
                     response = openai.Completion.create(
                         engine="gpt-3.5-turbo-instruct",
                         prompt=prompt,
                         temperature=1.0,
                         max_tokens=500
-                        )
+                    )
                     response = json.dumps(response, indent=2)
                     data = json.loads(response)
                     text = data['choices'][0]['text']
-                    response = text.encode('ascii', 'ignore').decode('ascii')
-                    print("respuesta del chatbot: ", response)
-                    response = str(response)
-                    output = True
-                    break
+                    response = f"{text}. Para una atención personalizada escríbenos al: {tel_link}"
+
+                    return Response({"message": response})
             else:
-                #generar un sistema de rol user tipo langchain pero con la base de conocimientos de keywords
-                #este conjunto de keywords son el contexto en el que el agente debe entender la entrada
-                
-                
-                prompt = f"construye una respuesta general amable y muy corta al estilo Tweet \
+                prompt = f"construye una respuesta general amable y muy corta e informativa al estilo Tweet \
                     tomando como contexto las palabras clave que existen en {keywords} \
-                    que sean mas similares a la {word} introducida\
+                    que sean más similares a la {word} introducida\
                     por el usuario sin mencionar las palabras clave y terminando con un emoji"
-                openai.api_key = "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"
+                openai.api_key = "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"
                 
                 response = openai.Completion.create(
                         engine="gpt-3.5-turbo-instruct",
                         prompt=prompt,
                         temperature=1.0,
                         max_tokens=100
-                        )
+                )
                 response = json.dumps(response, indent=2)
                 data = json.loads(response)
                 text = data['choices'][0]['text']
-                response = text.encode('ascii', 'ignore').decode('ascii')
-                print("respuesta del chatbot: ", response)
-                response = f'{response}'
-                output = True
-                break
-            
-    return Response({'message': response})
+                response = f"{text}. Para una atención personalizada escríbenos al: {tel_link}"
 
+                return Response({"message": response})
